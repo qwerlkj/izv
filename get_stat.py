77e18884236aef2ed6a5a45413d636f6ee0eaafa
Qwerlkj <qwerlkj@azet.sk>
2021-11-07 15:46:52 +0100

    Prepared plot

--- get_stat.py
+++ get_stat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import argparse
+
 import numpy as np
 import matplotlib.pyplot as plt
 # povolene jsou pouze zakladni knihovny (os, sys) a knihovny numpy, matplotlib a argparse
@@ -10,7 +12,12 @@ from download import DataDownloader
 def plot_stat(data_source,
               fig_location=None,
               show_figure=False):
-    pass
+    print("Plot stat")
 
 
 # TODO pri spusteni zpracovat argumenty
+if __name__ == '__main__':
+    print("argparse")
+    fig_location = None
+    show_figure = False
+    plot_stat(DataDownloader().get_dict(),fig_location, show_figure)
